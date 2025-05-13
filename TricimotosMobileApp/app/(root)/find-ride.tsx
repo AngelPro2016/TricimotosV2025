34575f4fe2776d8b0d71e0a4a42618e57acdfafe
AngelPro2016 <angelnavarret9@hotmail.com>
2025-05-13 08:28:20 -0500

    1.0.0 error reactmaps

---
+++ TricimotosMobileApp/app/(root)/find-ride.tsx
@@ -0,0 +1,53 @@
+import { router } from "expo-router";
+import { Text, View } from "react-native";
+
+import CustomButton from "@/components/CustomButton";
+import GoogleTextInput from "@/components/GoogleTextInput";
+import RideLayout from "@/components/RideLayout";
+import { icons } from "@/constants";
+import { useLocationStore } from "@/store";
+
+const FindRide = () => {
+  const {
+    userAddress,
+    destinationAddress,
+    setDestinationLocation,
+    setUserLocation,
+  } = useLocationStore();
+
+  return (
+    <RideLayout title="Viaje">
+      <View className="my-3">
+        <Text className="text-lg font-JakartaSemiBold mb-3">De</Text>
+
+        <GoogleTextInput
+          icon={icons.target}
+          initialLocation={userAddress!}
+          containerStyle="bg-neutral-100"
+          textInputBackgroundColor="#f5f5f5"
+          handlePress={(location) => setUserLocation(location)}
+        />
+      </View>
+
+      <View className="my-3">
+        <Text className="text-lg font-JakartaSemiBold mb-3">a</Text>
+
+        <GoogleTextInput
+          icon={icons.map}
+          initialLocation={destinationAddress!}
+          containerStyle="bg-neutral-100"
+          textInputBackgroundColor="transparent"
+          handlePress={(location) => setDestinationLocation(location)}
+        />
+      </View>
+
+      <CustomButton
+        title="Encontrar ahora"
+        onPress={() => router.push(`/(root)/confirm-ride`)}
+        className="mt-5"
+      />
+    </RideLayout>
+  );
+};
+
+export default FindRide;

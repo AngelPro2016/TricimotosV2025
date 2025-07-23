d813ed4f99f51e88e3c2b85109c6fd7b33f50299
AngelPro2016 <angelnavarret9@hotmail.com>
2025-07-23 13:49:13 -0500

    Apk

--- TricimotosMobileApp/constants/env.ts
+++ TricimotosMobileApp/constants/env.ts
@@ -1,2 +1,2 @@
 // constants/env.ts
-export const BASE_URL = "http://192.168.10.170:8000"; // cambia aquí cuando cambie la red
+export const BASE_URL = "https://tricimotos.up.railway.app"; // cambia aquí cuando cambie la red
